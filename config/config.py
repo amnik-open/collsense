a918f31d40cb0f852bf9adb100744f585378fa31
amnik-open <amnik.open@gmail.com>
2023-06-13 08:56:06 +0330

    config: add config module
    This module maintain and parse configurations of application.

---
+++ config/config.py
@@ -0,0 +1,21 @@
+import configparser
+
+
+class CollsenseConfig:
+
+    def __init__(self):
+        self.config = self._parse()
+
+    def _parse(self):
+        config = configparser.ConfigParser()
+        config.read("config.ini")
+        return config
+
+    def get_collector_config(self):
+       return self.config["Collector"]
+
+    def get_database_config(self):
+        return self.config["Database"]
+
+    def get_url_database_config(self):
+        return self.config["URL Database"]
