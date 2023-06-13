8cb711ad14fba5ce3cc00e221c6ab3b65574238a
amnik-open <amnik.open@gmail.com>
2023-06-13 08:54:36 +0330

    plugins: add sensor type one
    Add new sensor parser to collsense application.

---
+++ plugins/sensor_type_one.py
@@ -0,0 +1,19 @@
+from plugins.sensor_parser_interface import SensorParserInterface
+
+
+class SensorParser(SensorParserInterface):
+    type = "sensor_type_one"
+    sensor_output_json_schema = {
+        'type': 'object',
+        'properties': {
+            'temperature': {'type': 'number'},
+        },
+        'additionalProperties': False,
+    }
+
+    @classmethod
+    def parse(cls, json_data):
+        output = {"type": cls.type, 'temperature': json_data['temperature']}
+        return output
+
+
