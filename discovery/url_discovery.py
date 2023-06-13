002ba046fd6b24afbd8fc75142560252a0add10b
amnik-open <amnik.open@gmail.com>
2023-06-13 09:25:26 +0330

    discovery: add discovery module
    This module implement functionality about discovering target sensors
    to scrape.

---
+++ discovery/url_discovery.py
@@ -0,0 +1,34 @@
+import time
+import threading
+from db.rdb_interface import PostgresInterface
+
+
+class UrlDiscovery:
+
+    def __init__(self, pipeline, event):
+        self.db = PostgresInterface()
+        self.pipeline = pipeline
+        self.stop_event = event
+        self.sensor_url = {}
+
+    def _discover(self):
+        while not self.stop_event.is_set():
+            new_addresses = self.db.get_sensor_id_url()
+            for i, v in self.sensor_url.items():
+                if i in new_addresses:
+                    if v != new_addresses[i]:
+                        self.pipeline.publish_update_message(i,
+                                                             new_addresses[i])
+                        self.sensor_url[i] = new_addresses[i]
+                    del new_addresses[i]
+                else:
+                    self.pipeline.publish_delete_message(i, v)
+                    del self.sensor_url[i]
+            for i, v in new_addresses.items():
+                self.pipeline.publish_create_message(i, v)
+                self.sensor_url[i] = v
+            time.sleep(10)
+
+    def start(self):
+        discover = threading.Thread(target=self._discover)
+        discover.start()
