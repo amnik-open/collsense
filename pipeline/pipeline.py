05a6e080ca6740468becc67eb91a522899ff48e2
amnik-open <amnik.open@gmail.com>
2023-06-13 09:22:49 +0330

    pipeline: add pipeline module
    This module implement message queue for transfering messages about
    sensors life cycle.

---
+++ pipeline/pipeline.py
@@ -0,0 +1,25 @@
+from queue import Queue
+from collections import namedtuple
+
+Message = namedtuple('Message', ['name', 's_id', 'url'])
+
+
+class SensorMessagePipeline(Queue):
+
+    def __init__(self):
+        super().__init__()
+
+    def publish_update_message(self, s_id, url):
+        m = Message("update", s_id, url)
+        self.put(m)
+
+    def publish_create_message(self, s_id, url):
+        m = Message("create", s_id, url)
+        self.put(m)
+
+    def publish_delete_message(self, s_id, url):
+        m = Message("delete", s_id, url)
+        self.put(m)
+
+    def consume(self):
+        return self.get()
