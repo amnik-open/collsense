c4a80841fef9ada465d3e95b0d2b57daf24c970e
amnik-open <amnik.open@gmail.com>
2023-06-15 09:24:36 +0330

    plugins: add plugin for sensor_type_two

---
+++ plugins/sensor_type_two.py
@@ -0,0 +1,21 @@
+from plugins.sensor_parser_interface import SensorParserInterface
+
+
+class SensorParser(SensorParserInterface):
+    type = "sensor_type_two"
+    sensor_output_json_schema = {
+        'type': 'object',
+        'properties': {
+            'temperature': {'type': 'number'},
+            'humidity': {'type': 'number'},
+        },
+        'additionalProperties': False,
+    }
+
+    @classmethod
+    def parse(cls, json_data):
+        output = {"type": cls.type, 'temperature': json_data['temperature'],
+                  'humidity': json_data['humidity']}
+        return output
+
+
