bf7e71379f13fa35710b7306c5764f7fd80f3188
amnik-open <amnik.open@gmail.com>
2023-06-16 10:58:29 +0330

    collector: add status tag to sensor records
    With this change scraper set tag to show that sensor is UP or DOWN.
    Also undefined sensors are handled.

--- collector/collector_scraper.py
+++ collector/collector_scraper.py
@@ -13,29 +13,47 @@ class Scraper:
     def __init__(self, target, event):
         self.target = target
         self.stop_event = event
-        self.parser = None
+        self.sensor = None
+        self.interval = int(Conf.get_scraper_config()["scrape_interval"])
 
-    @staticmethod
-    def _create_sensor_schema(data):
-        type = data["type"]
-        del data["type"]
-        s = SensorSchema(measurement=type, tags={"loc": "here"},
-                         fields=data)
+    def _create_sensor_schema(self, data, tags):
+        if self.sensor is None or self.sensor == "undefined":
+            s = SensorSchema(measurement="undefined_sensor", tags=tags,
+                             fields=data)
+        else:
+            s = SensorSchema(measurement=self.sensor.type, tags=tags,
+                             fields=data)
         return s
 
     def _scrape(self):
+        timeout = int(Conf.get_scraper_config()["scrape_timeout"])
         while not self.stop_event.is_set():
-            response = requests.get(self.target.strip())
-            json_data = json.loads(response.text)
+            status = "UP"
             try:
-                data = self.parser(json_data)
-            except TypeError:
-                self.parser = SensorRecognition.get_sensor_parser(json_data)
-                data = self.parser(json_data)
-            s = self._create_sensor_schema(data)
+                response = requests.get(self.target.strip(), timeout=timeout)
+            except Exception as e:
+                status = "DOWN"
+            tags = {"url": self.target, "status": status}
+            data = {"NULL": "No Data"}
+            if self.sensor == "undefined":
+                tags["sensor"] = "Undefined"
+            else:
+                if status == "UP":
+                    json_data = json.loads(response.text)
+                    if self.sensor is None:
+                        try:
+                            self.sensor = SensorRecognition.get_sensor(json_data)
+                            data = self.sensor.parse(json_data)
+                        except ValueError:
+                            self.sensor = "undefined"
+                            tags["sensor"] = "Undefined"
+                            print("Sensor is Undefined")
+                    else:
+                        data = self.sensor.parse(json_data)
+
+            s = self._create_sensor_schema(data, tags)
             s.save()
-            interval = int(Conf.get_scraper_config()["scrape_interval"])
-            time.sleep(interval)
+            time.sleep(self.interval)
 
     def stop(self):
         self.stop_event.set()
