4b87b464cd36d8f03c9830427f1a2130f8997e9a
amnik-open <amnik.open@gmail.com>
2023-06-16 11:05:43 +0330

    db: add module to create mean tasks
    This module create tasks according to tasks_mean_period to calculating
    mean.

--- db/setup_db_tasks.py
+++ db/setup_db_tasks.py
@@ -9,35 +9,20 @@ class SetupTasks:
         self.db = InfluxdbInterface()
 
     def create_mean_tasks(self):
-        sensors = Conf.get_collector_config()["enabled_sensor"].split(",")
         db_conf = Conf.get_database_config()
-        try:
-            self.db.create_bucket(bucket_name="hourly_mean", org=db_conf[
-                "org"])
-            self.db.create_bucket(bucket_name="weekly_mean", org=db_conf[
-                "org"])
-            self.db.create_bucket(bucket_name="monthly_mean", org=db_conf[
-                "org"])
-        except:
-            print("created")
-        for sensor in sensors:
-            self.db.create_periodic_mean_task(period="1h",
-                                              measurement=sensor.strip(),
-                                              bucket_src=db_conf["bucket"],
-                                              bucket_dst="hourly_mean",
-                                              org=db_conf["org"],
-                                              task_name="hourly_mean")
-        for sensor in sensors:
-            self.db.create_periodic_mean_task(period="1w",
-                                              measurement=sensor.strip(),
-                                              bucket_src=db_conf["bucket"],
-                                              bucket_dst="weekly_mean",
-                                              org=db_conf["org"],
-                                              task_name="weekly_mean")
-        for sensor in sensors:
-            self.db.create_periodic_mean_task(period="1mo",
-                                              measurement=sensor.strip(),
-                                              bucket_src=db_conf["bucket"],
-                                              bucket_dst="monthly_mean",
-                                              org=db_conf["org"],
-                                              task_name="monthly_mean")
+        sensors = Conf.get_collector_config()["enabled_sensor"].split(",")
+        tasks_period = Conf.get_collector_config()["tasks_mean_period"].split(",")
+        for p in tasks_period:
+            try:
+                self.db.create_bucket(bucket_name=f"{p}_mean", org=db_conf[
+                    "org"])
+            except:
+                print(f"{p}_mean created")
+            for sensor in sensors:
+                self.db.create_periodic_mean_task(period=f"{p}",
+                                                  measurement=sensor.strip(),
+                                                  bucket_src=db_conf["bucket"],
+                                                  bucket_dst=f"{p}_mean",
+                                                  org=db_conf["org"],
+                                                  task_name=f"{p}_mean_" +
+                                                            sensor.strip())
