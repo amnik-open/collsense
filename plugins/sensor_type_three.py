5815e21f50198a8b4b424f8693754ab6e85d45bc
amnik-open <amnik.open@gmail.com>
2023-06-18 08:32:30 +0330

    plugins: add sensor_type_three plugin

---
+++ plugins/sensor_type_three.py
@@ -0,0 +1,24 @@
+from plugins.sensor_parser_interface import SensorParserInterface
+
+
+class SensorParser(SensorParserInterface):
+    type = "sensor_type_three"
+    sensor_output_json_schema = {
+        'type': 'object',
+        'properties': {
+            'parameters': {
+                'type': 'object',
+                "properties": {
+                    'temp': {"type": "number"},
+                    'hum': {"type": "number"},
+                },
+            }
+        },
+        'additionalProperties': False,
+    }
+
+    @classmethod
+    def parse(cls, json_data):
+        output = {'temperature': json_data['parameters']['temp'], 'humidity':
+            json_data['parameters']['hum']}
+        return output
