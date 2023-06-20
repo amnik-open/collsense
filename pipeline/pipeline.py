fb30c993f027d321c9baf530dfd59123c4d25616
amnik-open <amnik.open@gmail.com>
2023-06-20 10:10:22 +0330

    pipeline: add log to pipeline module

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -1,7 +1,9 @@
 from queue import Queue
 from collections import namedtuple
+from log.log import Logging
 
 Message = namedtuple('Message', ['name', 's_id', 'url'])
+Log = Logging.get_logger("pipeline")
 
 
 class SensorMessagePipeline(Queue):
@@ -12,18 +14,24 @@ class SensorMessagePipeline(Queue):
     def produce_update_message(self, s_id, url):
         m = Message("update", s_id, url)
         self.put(m)
+        Log.debug(f"{m} is produced")
 
     def produce_create_message(self, s_id, url):
         m = Message("create", s_id, url)
         self.put(m)
+        Log.debug(f"{m} is produced")
 
     def produce_delete_message(self, s_id, url):
         m = Message("delete", s_id, url)
         self.put(m)
+        Log.debug(f"{m} is produced")
 
     def produce_stop_message(self):
         m = Message("stop", None, None)
         self.put(m)
+        Log.debug(f"{m} is produced")
 
     def consume_message(self):
-        return self.get()
+        m = self.get()
+        Log.debug(f"{m} is consumed")
+        return m
