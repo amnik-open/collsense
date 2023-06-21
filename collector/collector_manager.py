2ffd104a4fdc645ac4cee2b15bb34403a593f1a1
amnik-open <amnik.open@gmail.com>
2023-06-21 10:47:00 +0330

    collector: refactor collector manager
    Refactor colllector manager to process new available or unavailable
    messages.

--- collector/collector_manager.py
+++ collector/collector_manager.py
@@ -6,46 +6,46 @@ Log = Logging.get_logger("collector.manager")
 
 
 class CollectorManager:
-    def __init__(self, pipeline, event):
+    def __init__(self, pipeline, event, id_scraper):
         self.pipeline = pipeline
         self.stop_event = event
-        self.id_sensor = {}
+        self.id_scraper = id_scraper
 
     def _create_scraper(self, s_id, url):
         stop = threading.Event()
         c = Scraper(url, stop)
-        self.id_sensor[s_id] = c
+        self.id_scraper[s_id] = c
         return c
 
     def _update_scraper(self, s_id, url):
-        self.id_sensor[s_id].stop()
+        self.id_scraper[s_id].stop()
         return self._create_scraper(s_id, url)
 
     def _delete_scraper(self, s_id):
-        self.id_sensor[s_id].stop()
-        del self.id_sensor[s_id]
+        self.id_scraper[s_id].stop()
+        del self.id_scraper[s_id]
 
     def _stop_scrapers(self):
-        for scraper in self.id_sensor.values():
+        for scraper in self.id_scraper.values():
             scraper.stop()
 
     def _consume_discovery_message(self):
         while not self.stop_event.is_set() or not self.pipeline.empty():
             message = self.pipeline.consume_message()
-            if message.name == "create":
-                c = self._create_scraper(message.s_id, message.url)
-                c.start()
-                Log.info(f"New scraper {message.s_id} with url "
-                          f"{message.url} is started")
-            elif message.name == "update":
-                c = self._update_scraper(message.s_id, message.url)
-                c.start()
-                Log.info(f"Scraper {message.s_id} updated with "
-                          f"url {message.url}")
-            elif message.name == "delete":
+            if message.name == "available":
+                if message.s_id not in self.id_scraper:
+                    c = self._create_scraper(message.s_id, message.url)
+                    c.start()
+                    Log.info(f"New scraper {message.s_id} with url "
+                             f"{message.url} is started")
+                elif self.id_scraper[message.s_id].get_target() != message.url:
+                    c = self._update_scraper(message.s_id, message.url)
+                    c.start()
+                    Log.info(f"Scraper {message.s_id} updated with "
+                             f"url {message.url}")
+            elif message.name == "unavailable":
                 self._delete_scraper(message.s_id)
-                Log.info(f"Scraper {message.s_id} with url {message.url} "
-                          f"is deleted")
+                Log.info(f"Scraper {message.s_id} is deleted")
             elif message.name == "stop":
                 self._stop_scrapers()
                 Log.info("All scrapers are stopped")
