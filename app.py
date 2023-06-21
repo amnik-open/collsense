62e14117be580a8a9c093c0a56499222c06675a7
amnik-open <amnik.open@gmail.com>
2023-06-21 17:19:59 +0330

    collsense: add multiple collector manager in collsense
    Add collector_manager_number config to specify number of collector manager
    in application.

--- app.py
+++ app.py
@@ -4,17 +4,24 @@ from pipeline.pipeline import SensorMessagePipeline
 from db.setup_db_tasks import SetupTasks
 from web.web import Web
 from log.log import Logging
+from config.config import CollsenseConfig
 import threading
+
+Conf = CollsenseConfig()
 
 if __name__ == "__main__":
     Logging.setup_log()
     try:
         stop = threading.Event()
         p = SensorMessagePipeline()
-        d = UrlDiscovery(p, stop)
+        id_scraper = {}
+        d = UrlDiscovery(p, stop, id_scraper)
         d.start()
-        c = CollectorManager(p, stop)
-        c.start()
+        manager_num = int(Conf.get_collector_config()[
+                              "collector_manager_number"])
+        for i in range(manager_num):
+            c = CollectorManager(p, stop, id_scraper)
+            c.start()
         s = SetupTasks()
         s.create_mean_tasks()
         w = Web()
