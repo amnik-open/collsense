0812e290e3f040f48b8670038cca8edfe536d707
amnik-open <amnik.open@gmail.com>
2023-06-21 10:44:33 +0330

    discovery: refactor discovery to specify available or unavailable sensors

--- discovery/url_discovery.py
+++ discovery/url_discovery.py
@@ -10,37 +10,29 @@ Log = Logging.get_logger("discovery")
 
 class UrlDiscovery:
 
-    def __init__(self, pipeline, event):
+    def __init__(self, pipeline, event, id_scraper):
         self.db = PostgresInterface()
         self.pipeline = pipeline
         self.stop_event = event
-        self.sensor_url = {}
+        self.id_scraper = id_scraper
 
     def _discover(self):
         page_size = int(Conf.get_url_database_config()['fetch_page_size'])
         interval = int(Conf.get_discovery_config()["discovery_interval"])
         while not self.stop_event.is_set():
-            new_addresses = {}
-            new_addresses_cursor = self.db.get_sensor_address_cursor()
+            available_addresses = set()
+            db_addresses_cursor = self.db.get_sensor_address_cursor()
             while True:
-                addresses = new_addresses_cursor.fetchmany(page_size)
+                addresses = db_addresses_cursor.fetchmany(page_size)
                 if not addresses:
+                    for k in self.id_scraper.keys():
+                        if k not in available_addresses:
+                            self.pipeline.produce_unavailable_message(k)
                     break
                 for address in addresses:
-                    new_addresses[str(address[0])] = address[1].strip()
-                for i, v in self.sensor_url.items():
-                    if i in new_addresses:
-                        if v != new_addresses[i]:
-                            self.pipeline.produce_update_message(i,
-                                                                 new_addresses[i])
-                            self.sensor_url[i] = new_addresses[i]
-                        del new_addresses[i]
-                    else:
-                        self.pipeline.produce_delete_message(i, v)
-                        del self.sensor_url[i]
-                for i, v in new_addresses.items():
-                    self.pipeline.produce_create_message(i, v)
-                    self.sensor_url[i] = v
+                    self.pipeline.produce_available_message(s_id=str(address[0]),
+                                                        url=address[1].strip())
+                    available_addresses.add(str(address[0]))
             Log.debug("URLs are discovered")
             time.sleep(interval)
 
